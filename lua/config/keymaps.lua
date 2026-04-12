ba6e145620a94afd727e9544965f4dc06e7069cf
Yousif Yacoub <yousif.yacoub@hotmail.com>
2026-04-12 18:35:26 +0200

    Add buffer navigation keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { silent = true })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<CR>", { silent = true })
