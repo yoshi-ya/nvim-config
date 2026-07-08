d9fbc73029629e92210c958d833958bb650f9705
Yousif Yacoub <yacoub@4soft.de>
2026-07-08 21:05:07 +0200

    Fix treesitter texobjects setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,20 +1,41 @@
--- ─── Window & Buffer Navigation ───────────────────────────────────────────────
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+-- ─── Treesitter: jump between functions ───────────────────────────────────────
+vim.keymap.set("n", "<C-j>", function()
+	require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+end, { desc = "Next function start" })
+vim.keymap.set("n", "<C-k>", function()
+	require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+end, { desc = "Prev function start" })
 
+-- ─── Treesitter: jump between classes ────────────────────────────────────────
+vim.keymap.set("n", "]c", function()
+	require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+end, { desc = "Next class start" })
+vim.keymap.set("n", "[c", function()
+	require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+end, { desc = "Prev class start" })
+
+-- ─── Buffer Navigation ───────────────────────────────────────────────
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { silent = true })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprev<CR>", { silent = true })
 
 -- ─── File Tree (NeoTree) ── keymaps live in neotree.lua keys={} for lazy-loading
 
 -- ─── Telescope ────────────────────────────────────────────────────────────────
-vim.keymap.set("n", "<leader>p", function() require("telescope.builtin").find_files() end, { desc = "Find files" })
-vim.keymap.set("n", "<leader><leader>", function() require("telescope.builtin").live_grep() end, { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", function() require("telescope.builtin").buffers() end, { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>fh", function() require("telescope.builtin").help_tags() end, { desc = "Help tags" })
-vim.keymap.set("n", "<leader>ds", function() require("telescope.builtin").diagnostics() end, { desc = "Workspace diagnostics" })
+vim.keymap.set("n", "<leader>p", function()
+	require("telescope.builtin").find_files()
+end, { desc = "Find files" })
+vim.keymap.set("n", "<leader><leader>", function()
+	require("telescope.builtin").live_grep()
+end, { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fb", function()
+	require("telescope.builtin").buffers()
+end, { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", function()
+	require("telescope.builtin").help_tags()
+end, { desc = "Help tags" })
+vim.keymap.set("n", "<leader>ds", function()
+	require("telescope.builtin").diagnostics()
+end, { desc = "Workspace diagnostics" })
 
 -- ─── Formatting (conform) ─────────────────────────────────────────────────────
 vim.keymap.set({ "n", "v" }, "<leader>f", function()
@@ -34,10 +55,18 @@ vim.keymap.set("n", "<leader>l", function()
 end, { desc = "Trigger linting for current file" })
 
 -- ─── Git (gitsigns) ───────────────────────────────────────────────────────────
-vim.keymap.set("n", "<leader>hp", function() require("gitsigns").preview_hunk() end, { desc = "Preview hunk" })
-vim.keymap.set("n", "<leader>hs", function() require("gitsigns").stage_hunk() end, { desc = "Stage hunk" })
-vim.keymap.set("n", "<leader>hu", function() require("gitsigns").undo_stage_hunk() end, { desc = "Undo stage hunk" })
-vim.keymap.set("n", "<leader>hb", function() require("gitsigns").blame_line() end, { desc = "Blame line" })
+vim.keymap.set("n", "<leader>hp", function()
+	require("gitsigns").preview_hunk()
+end, { desc = "Preview hunk" })
+vim.keymap.set("n", "<leader>hs", function()
+	require("gitsigns").stage_hunk()
+end, { desc = "Stage hunk" })
+vim.keymap.set("n", "<leader>hu", function()
+	require("gitsigns").undo_stage_hunk()
+end, { desc = "Undo stage hunk" })
+vim.keymap.set("n", "<leader>hb", function()
+	require("gitsigns").blame_line()
+end, { desc = "Blame line" })
 
 -- ─── LSP (buffer-local, applied on LspAttach) ─────────────────────────────────
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -50,13 +79,19 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		map("n", "K", vim.lsp.buf.hover, "LSP hover")
 		map("n", "<leader>gd", vim.lsp.buf.definition, "Go to definition")
 		map("n", "<leader>gD", vim.lsp.buf.declaration, "Go to declaration")
-		map("n", "<leader>gr", function() require("telescope.builtin").lsp_references() end, "LSP references")
+		map("n", "<leader>gr", function()
+			require("telescope.builtin").lsp_references()
+		end, "LSP references")
 		map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
 		map("n", "<leader>im", vim.lsp.buf.implementation, "Go to implementation")
 		map("n", "<leader>r", vim.lsp.buf.rename, "Rename symbol")
 		map("n", "<leader>lf", vim.lsp.buf.format, "LSP format")
-		map("n", "<leader>dn", function() vim.diagnostic.jump({ count = 1 }) end, "Next diagnostic")
-		map("n", "<leader>dp", function() vim.diagnostic.jump({ count = -1 }) end, "Prev diagnostic")
+		map("n", "<leader>dn", function()
+			vim.diagnostic.jump({ count = 1 })
+		end, "Next diagnostic")
+		map("n", "<leader>dp", function()
+			vim.diagnostic.jump({ count = -1 })
+		end, "Prev diagnostic")
 		map("n", "<leader>de", vim.diagnostic.open_float, "Show diagnostic")
 	end,
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,56 +1,39 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	lazy = false,
 	build = ":TSUpdate",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
-		local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
-		if not ok then
-			return
-		end
-
-		ts_configs.setup({
-			ensure_installed = { "python", "java", "javadoc", "lua", "bash", "json", "yaml", "javascript", "typescript", "tsx", "regex", "dockerfile", "xml", "ini", "html", "gitignore", "angular" },
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"python",
+				"java",
+				"javadoc",
+				"lua",
+				"bash",
+				"json",
+				"yaml",
+				"javascript",
+				"typescript",
+				"tsx",
+				"regex",
+				"dockerfile",
+				"xml",
+				"ini",
+				"html",
+				"gitignore",
+				"angular",
+			},
 			highlight = { enable = true },
 			indent = { enable = true },
 			incremental_selection = { enable = true },
-			rainbow = { enable = true },
 			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = { query = "@function.outer", desc = "Select outer function" },
-						["if"] = { query = "@function.inner", desc = "Select inner function" },
-						["ac"] = { query = "@class.outer", desc = "Select outer class" },
-						["ic"] = { query = "@class.inner", desc = "Select inner class" },
-						["aa"] = { query = "@parameter.outer", desc = "Select outer argument" },
-						["ia"] = { query = "@parameter.inner", desc = "Select inner argument" },
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]m"] = { query = "@function.outer", desc = "Next function start" },
-						["]c"] = { query = "@class.outer", desc = "Next class start" },
-					},
-					goto_next_end = {
-						["]M"] = { query = "@function.outer", desc = "Next function end" },
-						["]C"] = { query = "@class.outer", desc = "Next class end" },
-					},
-					goto_previous_start = {
-						["[m"] = { query = "@function.outer", desc = "Prev function start" },
-						["[c"] = { query = "@class.outer", desc = "Prev class start" },
-					},
-					goto_previous_end = {
-						["[M"] = { query = "@function.outer", desc = "Prev function end" },
-						["[C"] = { query = "@class.outer", desc = "Prev class end" },
-					},
-				},
+				select = { enable = true },
+				move = { enable = true, set_jumps = true },
 			},
 		})
-	end
+	end,
 }
