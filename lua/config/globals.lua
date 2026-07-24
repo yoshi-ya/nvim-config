1e3139171870ce892ed720eba07c8a7a58db6bf5
Yousif Yacoub <yacoub@4soft.de>
2026-07-24 12:45:55 +0200

    Show current line number

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,6 +4,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.relativenumber = true
+vim.opt.number = true
 
 vim.api.nvim_create_autocmd("FileType", {
 	callback = function(ev)
