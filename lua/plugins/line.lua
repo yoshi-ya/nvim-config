4cfce5ea5ef4b8194ae84fb90c15f8d0da66e660
Yousif Yacoub <yacoub@4soft.de>
2026-07-26 13:28:56 +0200

    Add diagnostics and Gitsigns to lualine

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -10,6 +10,37 @@ return {
 			options = {
 				theme = "catppuccin-mocha",
 			},
+			sections = {
+				lualine_b = {
+					"branch",
+					{
+						"diff",
+						source = function()
+							local gitsigns = vim.b.gitsigns_status_dict
+							if gitsigns then
+								return {
+									added = gitsigns.added,
+									modified = gitsigns.changed,
+									removed = gitsigns.removed,
+								}
+							end
+						end,
+					},
+				},
+				lualine_x = {
+					{
+						"diagnostics",
+						sources = { "nvim_diagnostic" },
+						sections = { "error", "warn", "info", "hint" },
+						symbols = { error = "✗ ", warn = "⚠ ", info = "ℹ ", hint = "➔ " },
+						colored = true,
+						always_visible = false,
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+			},
 		})
 	end,
 }
