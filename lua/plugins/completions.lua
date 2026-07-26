5099be983a2b769a031774493e7d22a007ba4cb3
Yousif Yacoub <yacoub@4soft.de>
2026-07-26 13:42:55 +0200

    Add emoji completions

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 return {
 	"saghen/blink.cmp",
+	dependencies = {
+		"moyiz/blink-emoji.nvim",
+	},
 	version = "*",
 	opts = {
 		keymap = {
@@ -8,6 +11,22 @@ return {
 			["<CR>"] = { "accept", "fallback" },
 		},
 		appearance = { nerd_font_variant = "mono" },
-		sources = { default = { "lsp", "path", "snippets", "buffer" } },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+			providers = {
+				emoji = {
+					module = "blink-emoji",
+					name = "Emoji",
+					score_offset = 15, -- Tune by preference
+					opts = {
+						insert = true, -- Insert emoji (default) or complete its name
+						---@type string|table|fun():table
+						trigger = function()
+							return { ":" }
+						end,
+					},
+				},
+			},
+		},
 	},
 }
