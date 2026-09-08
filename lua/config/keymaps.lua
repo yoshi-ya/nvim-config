16519a28c5546aef5a05e418364ecf17e4878f9e
Yousif Yacoub <yacoub@4soft.de>
2026-09-08 18:27:40 +0200

    Add keymaps for copying filename to clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,5 +17,18 @@ keymap.set("n", "<leader>nh", "<cmd>nohlsearch<CR>", { desc = "Clear search high
 -- Clipboard
 keymap.set("v", "<leader>y", '"+y', { desc = "Copy selection to system clipboard" })
 
+local function copy_path(modifier)
+	return function()
+		local path = vim.fn.expand("%:" .. modifier)
+		vim.fn.setreg("+", path)
+		vim.notify("Copied " .. path)
+	end
+end
+
+keymap.set("n", "<leader>yp", copy_path("p"), { desc = "Copy absolute file path" })
+keymap.set("n", "<leader>yr", copy_path("."), { desc = "Copy file path relative to cwd" })
+keymap.set("n", "<leader>yn", copy_path("t"), { desc = "Copy file name" })
+keymap.set("n", "<leader>yd", copy_path("h"), { desc = "Copy directory path" })
+
 -- Symbols
 keymap.set("i", "<leader>we", "✓", { desc = "Write a check mark ✓" })
