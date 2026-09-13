c6891d575bfd996dfe161adae311bd0c3116dda9
Yousif Yacoub <yacoub@4soft.de>
2026-09-13 14:02:16 +0200

    Fix alpha symbols

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -18,12 +18,12 @@ return {
 
 		-- Shortcut labels mirror the `keys` specs of neotree.lua and telescope.lua.
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  New file", "<cmd>ene<CR>"),
-			dashboard.button("SPC 1", "  Toggle file tree", "<cmd>Neotree toggle<CR>"),
+			dashboard.button("e", "  New file", "<cmd>ene<CR>"),
+			dashboard.button("SPC 1", "  Toggle file tree", "<cmd>Neotree toggle<CR>"),
 			dashboard.button("SPC f f", "󰱼  Find files", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC SPC", "  Live grep", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC f b", "  Find buffers", "<cmd>Telescope buffers<CR>"),
-			dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+			dashboard.button("SPC SPC", "  Live grep", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC f b", "  Find buffers", "<cmd>Telescope buffers<CR>"),
+			dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
 		}
 
 		return dashboard.opts
